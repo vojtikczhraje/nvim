884818a18f485e44d074bed25c16fdd7e238d178
vojtikczhraje <vojta271000@gmail.com>
2025-03-07 08:47:40 +0100

    feat: new colorscheme

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,10 @@
+return {
+  "rebelot/kanagawa.nvim",
+  config = function()
+    require("kanagawa").setup({
+      -- i cannot configure nothing
+    })
+
+    vim.cmd("colorscheme kanagawa-dragon")
+  end,
+}
